94c9e3c4d14865e602f52a458eea74436d1ae9b9
goDFatHeR26 <ad26081309@gmail.com>
2023-07-18 17:25:35 +0530

    changes

---
+++ src/Components/Vehiclebooking/Carcharges.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Carcharges = () => {
+    return(
+        <>
+            <section className="booking">
+                <div className="booking-header">
+                    <p>Vehicle Details</p>
+                </div>
+                <div className="car-details">
+                    
+                </div>
+
+            </section>
+        </>
+    )
+}
+
+export default Carcharges;
+
+
+
+
+
+
